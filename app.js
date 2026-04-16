4a16385bef18f200bbd3317c73aa7360d7f090ff
aryxnpawar <aryanpawar2535@gmail.com>
2026-04-16 08:09:54 +0530

    CI/CD check

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nexora India | Next-Gen Cloud Infrastructure</title>
+    <title>Nexora India | Next Gen Cloud Infrastructure</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;800&family=Lexend:wght@600;700&display=swap" rel="stylesheet">
     <style>
         :root {
